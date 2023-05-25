a762865becb8ad15c54dc65768e85d3d7096313e
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-25 21:19:18 +0800

    pseudocode

--- Calculator GUI.py
+++ Calculator GUI.py
@@ -1,1 +1,10 @@
 # Marquez, Francis Ivan B.,_BSCpE 1-5
+# Add Window
+# Add first number box
+# Add second number box
+# Buttons for operation
+    # Addition
+    # Subtraction
+    # Multiplication
+    # Division
+# Output Message
--- Calculator.py
+++ Calculator.py
@@ -1,1 +1,3 @@
 # Marquez, Francis Ivan B.,_BSCpE 1-5
+# Class
+# Define Functions/Operations
