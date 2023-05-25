571722546312ff45208943635652184d8175ddb0
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-25 21:34:13 +0800

    Added window size and titile

--- Calculator GUI.py
+++ Calculator GUI.py
@@ -1,10 +1,13 @@
 # Marquez, Francis Ivan B.,_BSCpE 1-5
 # Import
 from tkinter import *
-from ClassCalculator import Calculator
+from ClassCalculator import *
 
 # Add Window
 window = Tk()
+window.geometry("450x600")
+window.title("Two Numbers Calculator")
+
 # Add first number box
 # Add second number box
 # Buttons for operation
