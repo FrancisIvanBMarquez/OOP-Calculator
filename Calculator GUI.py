c178239a81265be4fd2201a3e1c7192b4dce2194
FrancisIvanBMarquez <francisivanmarquez@gmail.com>
2023-05-25 23:50:56 +0800

    added addition button

--- Calculator GUI.py
+++ Calculator GUI.py
@@ -10,6 +10,7 @@ window.title("Two Numbers Calculator")
 calculator_icon = PhotoImage(file='CalculatorIcon.png')
 window.iconphoto(True,calculator_icon)
 window.config(background="SeaGreen1")
+
 # Title Box
 title_box = Label(window, text="Two Numbers Calculator", font=("Calibri",29,"bold"),fg="white",bg="deep sky blue",
                   relief=RAISED, bd=15,padx=12,pady=15)
@@ -21,9 +22,12 @@ first_number_box.place(x=120,y=150)
 # Add second number box
 second_number_box = Entry(window, font=("Calibri",15),bd=5)
 second_number_box.place(x=120, y=200)
+
 # Buttons for operation
 
     # Addition
+addition_button = Button(window, text="Add")
+addition_button.place(x=100,y=250)
 
     # Subtraction
 
